b854a964defa84c361ef57d589e5426686ec3024
Alan Jack <60137518+aldojack@users.noreply.github.com>
2023-02-20 22:47:51 +0000

    Order component with props

--- delivery-app/src/App.jsx
+++ delivery-app/src/App.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import MapContainer from "./components/MapContainer";
 import Navbar from "./components/Navbar";
+import Order from "./components/Order";
+import orders from "./data/orders";
 
 function App() {
+  const [initialOrders, setInitalOrders] = useState(orders);
+  const  allOrderElements = initialOrders.map((order) => {
+    return <Order {...order} key={order.orderNumber}/>
+  })
+
   return (
     <div className="wrapper">
       <Navbar />
-      <MapContainer/>
+      <MapContainer />
+      <div className="w-full">
+        <div className="max-w-[1240px] w-full h-full mx-auto p-4 space-y-4">
+        {allOrderElements}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ delivery-app/src/components/Order.jsx
@@ -0,0 +1,28 @@
+import { BsFillPersonFill } from "react-icons/bs";
+
+export default function Order({address, customerName, delivered, location, orderNumber}) {
+  return (
+    <div className="rounded-xl shadow-lg bg-slate-200 flex md:flex-col max-w-[500px] mx-auto justify-between items-center p-4">
+    <BsFillPersonFill />
+    <div>
+      <p className="font-bold">{customerName}</p>
+      <p>{address}</p>
+      <p>0 mins away</p>
+    </div>
+    <div>
+      <button
+        type="button"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Accept
+      </button>
+      <button
+        type="button"
+        className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+      >
+        Decline
+      </button>
+    </div>
+  </div>
+  )
+}
