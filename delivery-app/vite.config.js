576a980653ba9323e6f3b3744e24c48691bfef3c
Alan Jack <60137518+aldojack@users.noreply.github.com>
2023-02-20 20:42:28 +0000

    first commit

---
+++ delivery-app/vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA(
+      {
+        manifest: {
+          icons: [
+            {
+              src: "images/pin-icon.png",
+              sizes: "192x192",
+              type: "image/png",
+              purpose: "any maskable"
+            }
+  
+          ]
+        },
+        registerType: 'autoUpdate',
+        devOptions: {
+          enabled: true
+        }
+      }
+      )
+  ],
+});
