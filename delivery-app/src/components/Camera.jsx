67d0fa7b136ebfe3db071b2c1913a3f835cef52a
Alan Jack <60137518+aldojack@users.noreply.github.com>
2023-03-09 21:14:19 +0000

    Camera added, now shows preview and some style changes

--- delivery-app/src/components/Camera.jsx
+++ delivery-app/src/components/Camera.jsx
@@ -1,14 +1,23 @@
-import React, { useRef, useState, forwardRef, useImperativeHandle } from 'react';
-import {FaCameraRetro} from 'react-icons/fa'
-import {MdCancel} from 'react-icons/md'
+import React, {
+  useRef,
+  useState,
+  forwardRef,
+  useImperativeHandle,
+} from "react";
+import { FaCameraRetro } from "react-icons/fa";
+import { MdCancel } from "react-icons/md";
+import { BsFillCheckCircleFill } from "react-icons/bs";
 
-const Camera = forwardRef(({ onCapture }, ref) => {
+const Camera = forwardRef(({ onCapture, disabled }, ref) => {
   const videoRef = useRef(null);
   const [isCameraOpen, setIsCameraOpen] = useState(false);
+  const [showPreview, setShowPreview] = useState(false);
+  const [capturedImage, setCapturedImage] = useState(null);
 
   function startCamera() {
-    setIsCameraOpen(true)
-    navigator.mediaDevices.getUserMedia({ video: true })
+    setIsCameraOpen(true);
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
       .then((stream) => {
         videoRef.current.srcObject = stream;
         videoRef.current.onloadedmetadata = () => {
@@ -16,7 +25,7 @@ const Camera = forwardRef(({ onCapture }, ref) => {
         };
       })
       .catch((error) => {
-        console.error('Error accessing media devices', error);
+        console.error("Error accessing media devices", error);
       });
   }
 
@@ -29,34 +38,85 @@ const Camera = forwardRef(({ onCapture }, ref) => {
       });
       videoRef.current.srcObject = null;
     }
-    setIsCameraOpen(false)
+    setIsCameraOpen(false);
   }
 
   function takePicture() {
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d');
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
 
     canvas.width = videoRef.current.videoWidth;
     canvas.height = videoRef.current.videoHeight;
     context.drawImage(videoRef.current, 0, 0);
 
-    const dataUrl = canvas.toDataURL('image/png');
-    onCapture(dataUrl);
-    setIsCameraOpen(false)
-    stopCamera();
+    const dataUrl = canvas.toDataURL("image/png");
+
+    // Show the captured image and provide options to retake or save
+    setIsCameraOpen(false);
+    setCapturedImage(dataUrl);
+    setShowPreview(true);
+  }
+
+  function retakePicture() {
+    setCapturedImage(null);
+    setShowPreview(false);
+    setIsCameraOpen(false);
+    takePicture();
+  }
+
+  function savePicture() {
+    // Update the state with the captured image
+    onCapture(capturedImage);
+    setShowPreview(false);
+    // stopCamera();
   }
 
   useImperativeHandle(ref, () => ({
-    startCamera: startCamera
+    startCamera: startCamera,
   }));
 
   return (
     <div>
-      <video ref={videoRef} autoPlay className={isCameraOpen ? 'block' : 'hidden'}></video>
-      <div className='flex'>
-        <FaCameraRetro size={34} onClick={isCameraOpen ? takePicture : startCamera} className=" cursor-pointer"/>
-        <MdCancel size={34} onClick={stopCamera} className=" cursor-pointer"/>
-      </div>
+      {showPreview ? (
+        <div>
+          <img src={capturedImage} alt="Captured" />
+          <div className="flex gap-4">
+            <FaCameraRetro
+              onClick={retakePicture}
+              className="cursor-pointer"
+              size={34}
+            />
+            <BsFillCheckCircleFill
+              onClick={savePicture}
+              className="cursor-pointer"
+              size={34}
+              style={{ color: "rgb(60, 179, 113)" }}
+            />
+          </div>
+        </div>
+      ) : (
+        <div>
+          <video
+            ref={videoRef}
+            autoPlay
+            className={isCameraOpen ? "block" : "hidden"}
+          ></video>
+          <div className={disabled ? "hidden" : "flex"}>
+            <button
+              type="button"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              onClick={isCameraOpen ? takePicture : startCamera}
+            >
+              {isCameraOpen ? "Take Picture" : "Confirm Delivery"}
+            </button>
+            <MdCancel
+              size={34}
+              onClick={stopCamera}
+              className=" cursor-pointer"
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 });
