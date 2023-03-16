6b25e408cc31e919576b7350fa9468c7c63152a0
Alan Jack <60137518+aldojack@users.noreply.github.com>
2023-03-16 13:59:16 +0000

    Added regular expression to filter button which now accepts more than 1 word

--- delivery-app/src/components/FilterButton.jsx
+++ delivery-app/src/components/FilterButton.jsx
@@ -1,7 +1,10 @@
 export default function FilterButton({name, isPressed, setFilter}) {
+    //Regular expression looks for any lowercase letter 
+    //followed immediately by an uppercase letter and inserts a space between them
+    const formattedName = name.replace(/([a-z])([A-Z])/g, '$1 $2');
     return (
         <button type="button" className={isPressed ? "px-4 btn rounded-lg bg-[#FF6161]  hover:bg-[#F00000] toggle-btn ease-in duration-100" : "btn px-4 rounded-lg bg-slate-200 hover:bg-slate-300 toggle-btn"} aria-pressed={isPressed} onClick={() => setFilter(name)}>
-            <span>{name}</span>
+            <span>{formattedName}</span>
         </button>
     )
 }
